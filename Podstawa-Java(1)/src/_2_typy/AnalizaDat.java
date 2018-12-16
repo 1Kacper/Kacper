40c57111bffcabd0be86a4a435eb9a3e3e1124a7
1Kacper <barca60@onet.pl>
2018-12-16 13:42:13 +0100

    Wiek ćwieczenie

---
+++ Podstawa-Java(1)/src/_2_typy/AnalizaDat.java
@@ -0,0 +1,36 @@
+package _2_typy;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class AnalizaDat {
+
+    public static void main(String[] args) {
+
+        Scanner wejscie = new Scanner(System.in);
+
+        System.out.println("Podaj rok, miesiac , dzien urodzenia");
+
+        String data = wejscie.nextLine();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate dataUrodzenia = LocalDate.parse(data, formatter);
+        LocalDate dzisiaj = LocalDate.now();
+        Period roznica = Period.between(dataUrodzenia, dzisiaj);
+
+        int wiek = roznica.getYears();
+        if(wiek<18){
+            System.out.println("Niepełnoletni");
+        }else if(wiek < 65){
+            System.out.println("wiek produktywny");
+        }else {
+            System.out.println("Wiek emerytalny");
+        }
+    }
+}
+
+
+
+
