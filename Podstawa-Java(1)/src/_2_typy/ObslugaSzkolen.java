16ad5cfbde86811927148ca655e36d9b21c2bc41
1Kacper <barca60@onet.pl>
2018-12-16 11:12:48 +0100

    Matematyka(BigDecimal)

--- Podstawa-Java(1)/src/_2_typy/ObslugaSzkolen.java
+++ Podstawa-Java(1)/src/_2_typy/ObslugaSzkolen.java
@@ -1,15 +1,22 @@
 package _2_typy;
 
-public class szkolenie {
+import java.math.BigDecimal;
 
+public class ObslugaSzkolen {
     public static void main(String[] args) {
+        Szkolenie szkolenieJava = new Szkolenie("Szkolenie Java", 10, new BigDecimal("1000"));
 
-     Dane Obslugowe = new Dane()
+        szkolenieJava.wyswietl();
 
+    BigDecimal przychodBrutto = szkolenieJava.obliczPrzychodBrutto();
+
+        System.out.println("Przychod ze szkolenia Brutto: "+ przychodBrutto);
+
+        BigDecimal przychodNetto = szkolenieJava.obliczPrzychodNetto();
+        System.out.println("Przychod ze szkolenia Netto: "+ przychodNetto);
     }
+
 }
-
-
 
 
 
--- Podstawa-Java(1)/src/_2_typy/Szkolenie.java
+++ Podstawa-Java(1)/src/_2_typy/Szkolenie.java
@@ -1,14 +1,17 @@
 package _2_typy;
 
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 
 public class Szkolenie {
 
-    double cena;
+    BigDecimal cena;
     int liczbaUczestnikow;
     String nazwa;
     LocalDate data;
+    double wynik;
 
     // 1. tworzenie nowego obiektu.
     // budowa konstruktora = (public/private) (nazwa) (lista_parametrow)
@@ -22,7 +25,7 @@ public class Szkolenie {
 
     // konstruktor parametryczny
 
-    public Szkolenie(String nazwa, int liczbaUczestnikow, double cena) {
+    public Szkolenie(String nazwa, int liczbaUczestnikow, BigDecimal cena) {
 
         System.out.println("Wykonał sie konstruktor parametryczny");
         this.nazwa = nazwa;
@@ -38,9 +41,33 @@ public class Szkolenie {
 //[public/private] [typ_zwracany] [nazwa] [lista_parametrow]
 
     public void wyswietl() {
-        System.out.println("Nazwa grupy= " + this.nazwa);
-        System.out.println("Cena= " + this.cena);
-        System.out.println("Liczba uczestnikow= " + this.liczbaUczestnikow);
+
+        System.out.println("Nazwa grupy = " + this.nazwa);
+        System.out.println("Cena = " + this.cena);
+        System.out.println("Liczba uczestnikow = " + this.liczbaUczestnikow);
 
     }
+    public BigDecimal obliczPrzychodBrutto(){
+
+
+                return cena.multiply(BigDecimal.valueOf(liczbaUczestnikow));
+
+    }
+
+    public BigDecimal obliczPrzychodNetto(){
+
+        BigDecimal vat = BigDecimal.ONE.add(StaleAplikacji.STAWKA_VAT);
+
+        return obliczPrzychodBrutto() .divide(vat,2, RoundingMode.HALF_UP);
+
+
+
+
+
+
+
+    }
+
+
+
 }
