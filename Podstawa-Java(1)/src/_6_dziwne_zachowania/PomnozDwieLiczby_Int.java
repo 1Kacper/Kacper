4562e5df8d471f3e95cd0e867f9b44aa13d8c0aa
1Kacper <barca60@onet.pl>
2019-01-19 10:37:27 +0100

    Pomnoz

---
+++ Podstawa-Java(1)/src/_6_dziwne_zachowania/PomnozDwieLiczby_Int.java
@@ -0,0 +1,29 @@
+package _6_dziwne_zachowania;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class PomnozDwieLiczby_Int {
+    public static void main(String[] args) {
+        Scanner wejscie = new Scanner(System.in);
+        wejscie.useLocale(Locale.US);
+        System.out.println("Podaj dwie liczby");
+        int arg1 = wejscie.nextInt();
+        int arg2 = wejscie.nextInt();
+
+        int wynik = arg1 * arg2;
+        System.out.println(wynik);
+
+        System.out.println(" Max int wynosi " + Integer.MAX_VALUE);
+        // jak obliczyc w sposob bezpieczny
+
+        try {
+            int wynik2 = Math.multiplyExact(arg1, arg2);
+            System.out.println("Wynik poprawny: " + wynik2);
+        } catch (ArithmeticException e) {
+            System.out.println("Wynik nie miesci sie w zakresie ");
+        }
+    }
+}
+
+
