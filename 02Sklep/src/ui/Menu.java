388703787cb884af83081a198a0e4314d90d1322
1Kacper <barca60@onet.pl>
2019-02-02 10:44:14 +0100

    Sklep

---
+++ 02Sklep/src/ui/Menu.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void wyswietl() {
+        Scanner wejscie = new Scanner(System.in);
+
+        System.out.println("1. Dodaj alkochol");
+        System.out.println("2. Usuń alkochol");
+        System.out.println("3. Wyświetl");
+        System.out.println("q. Wyjście");
+        String wybor;
+
+        do {
+
+
+            System.out.println("Podaj wybór");
+            wybor = wejscie.nextLine();
+
+
+            switch (wybor) {
+                case "1":
+                    System.out.println(" Dodaj ");
+                    break;
+                case "2":
+                    System.out.println(" Usuń ");
+                    break;
+                case "3":
+                    System.out.println(" Wyświetl");
+                    break;
+                case "q":
+                    System.out.println(" Wyjście");
+                     break;
+                   default:
+                       System.out.println("Zła opcja, wybierz coś innego");
+            }
+
+        } while (!wybor.equals(" q "));
+
+    }
+}
+
